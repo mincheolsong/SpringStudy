0268e54bbd217af5ffd1a9b19148366ff7d45af6
mincheolsong <thdalscjf05@naver.com>
2023-04-18 10:46:48 +0900

    add spring ws

---
+++ src/spring/di/Program.java
@@ -0,0 +1,32 @@
+package spring.di;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import spring.di.entity.Exam;
+import spring.di.entity.NewlecExam;
+import spring.di.ui.ExamConsole;
+import spring.di.ui.GridExamConsole;
+import spring.di.ui.InlineExamConsole;
+
+public class Program {
+	// 9강 까지 들음
+	public static void main(String[] args) {
+		
+		/*
+		Exam exam = new NewlecExam();
+		ExamConsole console = new GridExamConsole(exam);
+		
+		console.setExam(exam);
+		*/
+		
+		ApplicationContext context = 
+				new ClassPathXmlApplicationContext("spring/di/setting.xml");
+		
+		//ExamConsole console = (ExamConsole) context.getBean("console");
+		
+		ExamConsole console = context.getBean(ExamConsole.class);
+		console.print();
+	}
+
+}
